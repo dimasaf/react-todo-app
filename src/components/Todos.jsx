d426228d683e8e4ae8604c07f4d7a388fa4e0081
dimasaf_ <dimas.firmana@ist.id>
2024-06-07 21:22:43 +0700

    Develop Toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,16 @@
 import TodoItem from "./TodoItem";
 
-function Todos({ todos }) {
+function Todos({ todos, toggleCompleted }) {
   return (
     <div style={styles.container}>
       {todos.map((todo) => (
-        <TodoItem title={todo.title} key={todo.id} />
+        <TodoItem
+          completed={todo.completed}
+          title={todo.title}
+          key={todo.id}
+          id={todo.id}
+          toggleCompleted={toggleCompleted}
+        />
       ))}
     </div>
   );
